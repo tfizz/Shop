9ae16d236678de802c0e7761a78ef1a3214fae5b
Babatunde Bamgbose <bamgbose.ab@gmail.com>
2019-01-14 22:35:06 -0500

    documentation added

---
+++ root/documentation/api_project.js
@@ -0,0 +1,20 @@
+define({
+  "name": "API Documentation",
+  "version": "0.0.1",
+  "description": "Demo Shop Documentation",
+  "title": "Demo Shop Documentation",
+  "url": "{{ base_url }}/api",
+  "template": {
+    "withCompare": false,
+    "withGenerator": false
+  },
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2019-01-15T03:30:03.689Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.6"
+  }
+});
